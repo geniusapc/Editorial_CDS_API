c6c6d243c6d5e7d8d9a4a32d7792ef0f3eebbfe5
cmcWebCode40 <cmcwebcode.dev@yahoo.com>
2020-01-11 10:17:51 +0100

    added fontawesome

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+     faEnvelope,
+     faKey,
+     faHome,
+     faPlusCircle,
+     faTrash,
+     faEdit,
+     faUser,
+     faAddressCard,
+     faTimesCircle,
+     faServer,
+     faSpinner,
+     faInfoCircle
+} from "@fortawesome/free-solid-svg-icons";
 
+import Layouts from "./layouts/Layouts";
+import "./sass/custom.scss";
 
-// import './sass/style.scss'
-import Layouts from './layouts/Layouts';
-import './sass/custom.scss';
-// import './sass/_theme.scss'
-// import './sass/_config.scss';
-
-
-// library.add(faEnvelope, faKey, faHome, faPlusCircle, faTrash, faEdit, faUser, faAddressCard, faTimesCircle, faServer, faSpinner, faInfoCircle);
-
+library.add(
+     faEnvelope,
+     faKey,
+     faHome,
+     faPlusCircle,
+     faTrash,
+     faEdit,
+     faUser,
+     faAddressCard,
+     faTimesCircle,
+     faServer,
+     faSpinner,
+     faInfoCircle
+);
 
 const App = () => {
-     
      return (
-
           <>
-             <Layouts/>
+               <Layouts />
           </>
-
      );
-
-}
+};
 
 export default App;
-
-
-
