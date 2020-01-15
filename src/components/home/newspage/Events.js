6b903e2e1aed28e0310c004394c294ca45850c3a
cmcWebCode40 <infinity_michael@yahoo.com>
2020-01-15 22:30:17 +0100

    added spinner features

--- src/components/home/newspage/Events.js
+++ src/components/home/newspage/Events.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import EventList from "./EventList";
 import Pagination from "../../pagination/Pagination";
+import Loader from "../../../shared/loader/Loader";
 
 const Events = props => {
      const news = {
@@ -8,21 +9,21 @@ const Events = props => {
           //                id: 1,
           //                title: " News update",
           //                text:
-          //                     "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international non-governmental organisation. The bill, which focuses on effective management solutions for overfishing, passed its first assessment by the",
+          //                     "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international ",
           //                img: "https://source.unsplash.com/user/erondu/1600x900"
           //           },
           //           {
           //                id: 2,
           //                title: " News update",
           //                text:
-          //                     "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international non-governmental organisation. The bill, which focuses on effective management solutions for overfishing, passed its first assessment by the",
+          //                     "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international ",
           //                img: "https://source.unsplash.com/user/erondu/1600x900"
           //           },
           //           {
           //                id: 3,
           //                title: " News update",
           //                text:
-          //                     "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international non-governmental organisation. The bill, which focuses on effective management solutions for overfishing, passed its first assessment by the",
+          //                     "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international ",
           //                img: "https://source.unsplash.com/user/erondu/1600x900"
           //           }
      };
@@ -30,46 +31,49 @@ const Events = props => {
      // "https://jsonplaceholder.typicode.com/posts";
 
      const [newsItem, setNewsItem] = useState([
-          {
-               id: 1,
-               title: " News update",
-               text:
-                    "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international non-governmental organisation. The bill, which focuses on effective management solutions for overfishing, passed its first assessment by the",
-               img: "https://source.unsplash.com/user/erondu/1600x900"
-          },
-          {
-               id: 2,
-               title: " News update",
-               text:
-                    "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international non-governmental organisation. The bill, which focuses on effective management solutions for overfishing, passed its first assessment by the",
-               img: "https://source.unsplash.com/user/erondu/1600x900"
-          },
-          {
-               id: 3,
-               title: " News update",
-               text:
-                    "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international non-governmental organisation. The bill, which focuses on effective management solutions for overfishing, passed its first assessment by the",
-               img: "https://source.unsplash.com/user/erondu/1600x900"
-          }
+          // {
+          //      id: 1,
+          //      title: " News update",
+          //      text:
+          //           "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international ",
+          //      img: "https://source.unsplash.com/user/erondu/1600x900"
+          // },
+          // {
+          //      id: 2,
+          //      title: " News update",
+          //      text:
+          //           "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international ",
+          //      img: "https://source.unsplash.com/user/erondu/1600x900"
+          // },
+          // {
+          //      id: 3,
+          //      title: " News update",
+          //      text:
+          //           "Belize's latest commitment to ocean conservation is its new Fisheries Resources Bill, for which the country partnered with the Environmental Defense Fund (EDF), an international ",
+          //      img: "https://source.unsplash.com/user/erondu/1600x900"
+          // }
      ]);
      const [currentPage, setCurrentPage] = useState(1);
+     const [loader, setLoader] = useState(false);
      const [postPerpage] = useState(6);
 
      const fetchEvents = async () => {
           const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+          setLoader(true);
           const data = await res.json();
           console.log(data);
           setNewsItem(data);
+          setLoader(false);
           console.log(newsItem);
      };
 
      const paginate = pageNumber => setCurrentPage(pageNumber);
 
-     // useEffect(() => {
-     //      fetchEvents();
-     // }, [paginate]);
+     useEffect(() => {
+          fetchEvents();
+     }, []);
 
-     // Get the Current Page
+     //Get the Current Page
 
      const indexOfLastPost = currentPage * postPerpage;
      const indexOfFirstPage = indexOfLastPost - postPerpage;
@@ -85,14 +89,18 @@ const Events = props => {
                     </div>
                </div>
                <div className="container mx-auto event-grid">
-                    {currentPosts.map(news => (
-                         <EventList
-                              key={news.id}
-                              title={news.title}
-                              image={news.img}
-                              text={news.text}
-                         />
-                    ))}
+                    {loader ? (
+                         <Loader />
+                    ) : (
+                         currentPosts.map(news => (
+                              <EventList
+                                   key={news.id}
+                                   title={news.title}
+                                   image={news.img}
+                                   text={news.text}
+                              />
+                         ))
+                    )}
                     <Pagination
                          className=" center"
                          postPerPage={postPerpage}
