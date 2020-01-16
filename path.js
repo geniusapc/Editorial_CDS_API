48f8a040b6305ddac63d24d903346cc184547809
youngenius <princearthur06@gmail.com>
2020-01-16 20:21:14 +0100

    created gallery model and api

--- path.js
+++ path.js
@@ -2,4 +2,4 @@ const pathe = require("path");
 module.exports = path = {
   postFolder: pathe.join(__dirname, "public", "img", "post"),
   galleryFolder: pathe.join(__dirname, "public", "img", "gallery")
-};
+}; 
