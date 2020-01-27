a665a3b5227568db35de307a595b7ed8ba1cbccf
youngenius <princearthur06@gmail.com>
2020-01-27 21:55:40 +0100

    ghgfygf

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const app = require('express')();
-const config = require('./config');
-const path = require('path');
+const express = require("express");
+const app = express();
+const config = require("./config");
+const path = require("path");
 
-require('./middleware')(app);
-require('./api')(app);
+require("./middleware")(app);
+require("./api")(app);
 
-if(process.env.NODE_ENV === "production"){
-	app.use(express.static('client/build'));
-	app.get('*', (req, res)=>{
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(config.port, () => {
-	console.log(`listening to port ${config.port}`);
+  console.log(`listening to port ${config.port}`);
 });
