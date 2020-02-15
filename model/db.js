2789f918fccd167ad4f6375cf544736919390019
youngenius <princearthur06@gmail.com>
2020-02-15 14:46:33 +0100

    sequalize online error

--- model/db.js
+++ model/db.js
@@ -1,4 +1,15 @@
 const config = require("../config");
-let db = config.db
-const {Sequelize} = require('sequelize')
-module.exports = new Sequelize(db.database, db.user, db.password, {dialect:'postgres'})
+let db = config.db;
+const { Sequelize } = require("sequelize");
+if (process.env.NODE_ENV == "production") {
+  module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: true //false
+  });
+} else {
+  module.exports = new Sequelize(db.database, db.user, db.password, {
+    dialect: "postgres",
+    logging: false
+  });
+}
